81a07bbfa72ae324bd0414ff8cbcfe92de5b8492
Henri Niva <henri.niva@aalto.fi>
2017-06-07 18:41:39 +0300

    Use express.js for rest api

--- server/api.js
+++ server/api.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env nodejs
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080, 'localhost');
-console.log('API server running at http://localhost:8080/');
+var express = require('express');
+var app = express();
+
+app.get('/api/hello', function (req, res) {
+    res.end( "Hello World!" );
+})
+
+var server = app.listen(8080, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('API server running at http://localhost:8080/');
+});
