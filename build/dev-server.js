a52e1f9313ab75062b2e2ed71348921788a2028b
Henri Niva <henri.niva@aalto.fi>
2017-06-07 18:21:43 +0300

    Launch dev node server + api server concurrently

--- build/dev-server.js
+++ build/dev-server.js
@@ -11,6 +11,7 @@ var express = require('express')
 var webpack = require('webpack')
 var proxyMiddleware = require('http-proxy-middleware')
 var webpackConfig = require('./webpack.dev.conf')
+var proxy = require('http-proxy-middleware');
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
@@ -21,6 +22,9 @@ var autoOpenBrowser = !!config.dev.autoOpenBrowser
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+app.use('/api', proxy({target: 'http://localhost:8080'}));
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
